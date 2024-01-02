0c7d3620d8676b07d24944dca9fe001bc6af0d1a
Stanislav Dudnik <136478699+idudnik@users.noreply.github.com>
2024-01-02 17:33:21 +0200

    v3

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,15 @@
+import os
+
 import pytest
+from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selene import browser
 
 from utils import attach
-
+@pytest.fixture(scope='session', autouse=True)
+def load_env():
+    load_dotenv()
 
 @pytest.fixture(scope='function', autouse=True)
 def browser_management():
@@ -19,8 +24,10 @@ def browser_management():
     }
 
     options.capabilities.update(selenoid_capabilities)
+    login = os.getenv('LOGIN')
+    password = os.getenv('PASSWORD')
     driver = webdriver.Remote(
-        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
         options=options)
 
     browser.config.driver = driver
